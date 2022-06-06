d108da7bdde2a05b7eb66c25a573625d2652eae8
hungvu <hungvu@fortna.com>
2022-06-06 19:38:30 +0700

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -28,7 +29,7 @@ mongoose.connect('mongodb://localhost:27017/shoesDB', options).then(
     console.log('Connection failed. Error: ${err}');
   }
 );
-
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
